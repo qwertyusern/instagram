4807c178bf32f0c3d1f93fee5cfe32947dfd81f0
qwertyusern <xojiakbargoipov4@gmail.com>
2022-08-10 23:41:06 +0500

    uyga vazifa

--- app1/views.py
+++ app1/views.py
@@ -7,9 +7,26 @@ class Posts(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSer
 
+class PostView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSer
+    def perform_update(self, serializer):
+        p=Profil.objects.get(user=self.request.user)
+        q=Post.objects.filter(profile=p)
+        if serializer.q is True:
+            serializer.update()
+        return Response(serializer)
+    def perform_destroy(self, instance):
+        p1=Profil.objects.get(username=self.request.user)
+        q = Post.objects.filter(profile=p1)
+        if q is True:
+            instance.delete()
+        return Response(instance)
+
 class Medias(generics.ListCreateAPIView):
     queryset = Media.objects.all()
     serializer_class = MediaSer
+
 
 class MediaGet(generics.RetrieveAPIView):
     queryset = Media.objects.all()
